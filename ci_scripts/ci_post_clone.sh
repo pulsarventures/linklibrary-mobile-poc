6a326192de571b3c9be36c91d1bd8d278e3690b4
balaji1359 <nagisetty.balaji@gmail.com>
2025-07-07 11:09:14 +0200

    Make ci_scripts executable

--- ci_scripts/ci_post_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Running post-clone script"
-cd ios
-pod install --repo-update
-echo "Pods installed"
--- ci_scripts/ci_pre_xcodebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Running pre-xcodebuild script"
-cd ios
-pod install
-echo "Pods ready"
